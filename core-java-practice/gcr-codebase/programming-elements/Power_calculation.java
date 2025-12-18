0e08529759fcb0c3c474d85604492bac6b6cea2b
“Ankit <ankitchaudhary5168033@gmail.com>
2025-12-18 12:02:12 +0530

    [Ankit Chaudhary]: Power Calculation

---
+++ core-java-practice/gcr-codebase/programming-elements/Power_calculation.java
@@ -0,0 +1,22 @@
+import java.util.Scanner; 
+
+public class Power_calculation {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in); 
+
+        //  base input
+        System.out.print("Enter base number: ");
+        double base = sc.nextDouble();
+
+        // exponent 
+        System.out.print("Enter exponent: ");
+        double exponent = sc.nextDouble();
+
+    
+        double result = Math.pow(base, exponent);
+
+        // output
+        System.out.println("Result: " + result);
+    }
+}
