64cb222b12a94ca2360180d6f20856d77da14c1f
“Ankit <ankitchaudhary5168033@gmail.com>
2025-12-18 11:26:32 +0530

    [Ankit Chaudhary]: Add two number

---
+++ core-java-practice/gcr-codebase/programming-elements/Add_number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Add_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         //take a input of first number
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+ 
+        //take inout of second number
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+        //sum of two number
+        System.out.println(sum);
+    }
+}
