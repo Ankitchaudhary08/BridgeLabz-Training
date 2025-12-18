dacfae60af30065f61c2b2529d46e30c61e542de
“Ankit <ankitchaudhary5168033@gmail.com>
2025-12-18 12:35:16 +0530

    [Ankit Chaudhary]: Area of circle

---
+++ core-java-practice/gcr-codebase/programming-elements/Area_of_circle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; // Import Scanner for input
+
+public class Area_of_circle {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in); 
+
+        // radius 
+        System.out.print("Enter radius of the circle: ");
+        double radius = sc.nextDouble();
+
+      
+        // Area = π × r × r
+        double area = Math.PI * radius * radius;
+
+        //output
+        System.out.println("Area of Circle: " + area);
+    }
+}
