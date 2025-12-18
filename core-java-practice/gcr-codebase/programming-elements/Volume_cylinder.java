d8d059306925aff51b2d29df8260796925d26714
“Ankit <ankitchaudhary5168033@gmail.com>
2025-12-18 11:46:07 +0530

    [Ankit Chaudhary]:Volme of Cylinder

---
+++ core-java-practice/gcr-codebase/programming-elements/Volume_cylinder.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; 
+
+public class Volume_cylinder {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in); 
+
+        // radius of cylinder
+        System.out.print("Enter radius of cylinder: ");
+        double radius = sc.nextDouble();
+
+        // height of cylinder
+        System.out.print("Enter height of cylinder: ");
+        double height = sc.nextDouble();
+
+        
+        // Volume = π × r × r × h
+        double volume = Math.PI * radius * radius * height;
+
+        // result
+        System.out.println("Volume of Cylinder: " + volume);
+    }
+}
