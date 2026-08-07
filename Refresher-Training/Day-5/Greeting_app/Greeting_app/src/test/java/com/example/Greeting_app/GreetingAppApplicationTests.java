72e355d746433a0e794931d295a2c708bf6c7777
“Ankit <ankitchaudhary5168033@gmail.com>
2026-08-07 13:51:34 +0530

    Added Greeting-App

---
+++ Refresher-Training/Day-5/Greeting_app/Greeting_app/src/test/java/com/example/Greeting_app/GreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Greeting_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
